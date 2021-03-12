248badfc16a6bed4a4347f0b82502d9f7c23ea73
Nicolae <nicolae.ursu@indrivo.com>
2021-03-12 15:42:02 +0200

    Finish lab 2

--- Lab2/Lab2/Program.cs
+++ Lab2/Lab2/Program.cs
@@ -14,7 +14,7 @@ namespace Lab2
     }
     class Program
     {
-        static readonly string textFile = @"C:\Users\colea\Documents\LFPCLabs\Lab2\Lab2\Varianta9.txt";
+        static readonly string textFile = @"C:\Users\nicolae.ursu\Documents\LFPCLabs\Lab2\Lab2\Varianta9.txt";
         static string[] Q;
         static string[] E;
         static string[] start;
@@ -47,7 +47,7 @@ namespace Lab2
                             break;
                         case 4:
                             start = ln.Split(" ");
-                            foreach(var s in start)
+                            foreach (var s in start)
                             {
                                 Transitions.Enqueue(s);
                             }
@@ -71,7 +71,7 @@ namespace Lab2
                 file.Close();
             }
         }
-        
+
         public void ConverNFAToDFA()
         {
             while (Transitions.Any())
@@ -83,15 +83,28 @@ namespace Lab2
                 for (int i = 0; i < E.Length; i++)
                 {
                     var currentTransition = new List<Node>();
+                    var trans = Transitions.Peek().Split('-');
+                    if (trans.Count() > 1)
+                    {
+                        foreach (var tr in trans)
+                        {
+                            if (tr != "DEAD")
+                            {
+                                foreach(var transition in NFA[tr].Where(x => x.Value == E[i]).ToList())
+                                    currentTransition.Add(transition);
+                            }
+                        }
+                    }
                     if (NFA.ContainsKey(Transitions.Peek()))
                     {
 
-                     currentTransition = NFA[Transitions.Peek()].Where(x => x.Value == E[i]).ToList();
+                        currentTransition = NFA[Transitions.Peek()].Where(x => x.Value == E[i]).ToList();
                     }
                     //-----new q2q3 reunion need to be made
                     if (currentTransition.Count() > 0)
                     {
-                        var newState = String.Concat(currentTransition.Select(x => x.NextState));
+                        var newState = String.Concat(currentTransition.Select(x => x.NextState + "-"));
+                        newState = newState.TrimEnd('-');
                         DFA[Transitions.Peek()].Add(new Node
                         {
                             NextState = newState,
@@ -121,11 +134,103 @@ namespace Lab2
             }
         }
 
+        /// <summary>
+        /// DEAD means this is a dead state
+        /// -> point to the start positions
+        /// * point to the final positions
+        /// </summary>
+        public void PrindDFA()
+        {
+            for (int i = 0; i < E.Length; i++)
+            {
+                Console.Write("\t\t");
+                Console.Write(E[i]);
+            }
+            Console.WriteLine();
+            foreach(var transition in DFA)
+            {
+                if (transition.Key.Contains("-"))
+                {
+                    var transitions = transition.Key.Split("-");
+                    var printed = false;
+                    foreach(var tr in transitions)
+                    {
+                        if (start.Contains(tr) && final.Contains(tr))
+                        {
+                            Console.Write("->*");
+                            foreach (var startEndTransition in transitions)
+                                Console.Write(startEndTransition);
+                            printed = true;
+                            break;
+                        }
+                        else if (start.Contains(tr))
+                        {
+                            Console.Write("->");
+                            foreach (var startEndTransition in transitions)
+                                Console.Write(startEndTransition);
+                            printed = true;
+                            break;
+                        }
+                        else if (final.Contains(tr))
+                        {
+                            Console.Write("*");
+                            foreach (var startEndTransition in transitions)
+                                Console.Write(startEndTransition);
+                            printed = true;
+                            break;
+                        }
+                    }
+                    if(!printed)
+                    {
+                        foreach (var startEndTransition in transitions)
+                            Console.Write(startEndTransition);
+                    }
+                }
+                else
+                {
+                    if (start.Contains(transition.Key) && final.Contains(transition.Key))
+                    {
+                        Console.Write($"->*{transition.Key}");
+
+                    }
+                    if (start.Contains(transition.Key))
+                    {
+                        Console.Write($"->{transition.Key}");
+
+                    }
+                    else if (final.Contains(transition.Key))
+                    {
+                        Console.Write($"*{transition.Key}");
+                    }
+                    else
+                    {
+                        Console.Write($"{transition.Key}");
+                    }
+                }
+                
+                for (int j = 0; j < E.Length; j++)
+                {
+                    foreach (var tr in transition.Value)
+                    {
+                        if (tr.Value == E[j])
+                        {
+                            Console.Write("\t\t");
+                            Console.Write(tr.NextState);
+                        }
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+
+        
         static void Main(string[] args)
         {
             var program = new Program();
             program.Parse();
             program.ConverNFAToDFA();
+            program.PrindDFA();
+            
         }
     }
 }
